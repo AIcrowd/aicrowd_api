9f072a0226a1b130894166c6f2dc523c128e32cc
SP Mohanty <spmohanty91@gmail.com>
2018-04-25 01:57:34 +0200

    Fix linter complaints

--- tests/test_create_submission.py
+++ tests/test_create_submission.py
@@ -14,10 +14,11 @@ try:
     AUTH_TOKEN = os.environ["AUTH_TOKEN"]
     EXAMPLE_API_KEY = os.environ["EXAMPLE_API_KEY"]
 except Exception as e:
-    raise Exception("Environment variables do not seem to be set
+    raise Exception("Environment variables do not seem to be set. "
                     + "Please set the following env variables: "
                     + ",".join(EXPECTED_KEYS)
                     )
+
 
 def test_successfully_creates_submission_with_correct_api_key():
     """Tests if Successfully creates submission for correct api_key \
@@ -31,6 +32,7 @@ def test_successfully_creates_submission_with_correct_api_key():
     assert submission.score_secondary is False
     assert submission.grading_status is "submitted"
 
+
 def test_throws_error_when_not_authenticated():
     """Tests if throws error when creating submission without authentication"""
     api = CROWDAI_API(AUTH_TOKEN)
@@ -38,8 +40,10 @@ def test_throws_error_when_not_authenticated():
     with pytest.raises(CrowdAIAPIException):
         api.create_submission(challenge_id)
 
+
 def test_throws_error_when_wrong_challenge_id():
-    """Tests if throws error when creating submission with wrong challenge id"""
+    """Tests if throws error when creating submission with \
+    wrong challenge id"""
     api = CROWDAI_API(AUTH_TOKEN)
     api.authenticate_participant(EXAMPLE_API_KEY)
     challenge_id = "wrong_challenge_id"
