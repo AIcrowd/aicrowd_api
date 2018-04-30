87f595a8fdb7009b13cb47bdf536e7b165e98a12
SP Mohanty <spmohanty91@gmail.com>
2018-04-30 20:11:59 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/crowdai/crowdai_api',
-    version='0.1.0.dev1',
+    version='0.1.1.dev1',
     zip_safe=False,
 )
