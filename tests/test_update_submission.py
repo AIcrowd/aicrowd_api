d7a3ea83bcd4d9e41ec85124695ce366f16bf6c4
SP Mohanty <spmohanty91@gmail.com>
2018-05-27 18:31:08 +0200

    Update tests

--- tests/test_update_submission.py
+++ tests/test_update_submission.py
@@ -80,6 +80,54 @@ def test_updates_submission_with_meta():
     assert submission_from_server.meta["something"] == "A_1"
     assert submission_from_server.meta["something_else"] == "B_1"
 
+## This test fails right in the first block where the submission object on a new
+# create submission mysteriously has the meta param assigned from the previous test
+# This needs further investigation
+# 
+# def test_updates_submission_with_meta_with_metaoverwrite_forced_False():
+#     """ Tests is it successfully updates submission with the
+#         meta_overwrite param = False
+#     """
+#     api = CROWDAI_API(AUTH_TOKEN)
+#     challenge_id = "test_challenge"
+#     api.authenticate_participant(EXAMPLE_API_KEY)
+#     submission = api.create_submission(challenge_id)
+#     assert submission.meta == {}
+
+    # assert type(submission_new.id) == int
+    # assert submission_new.score is False
+    # assert submission_new.score_secondary is False
+    # assert submission_new.grading_status is "submitted"
+    # assert submission_new.meta == {}
+
+    # submission.score = 0.98
+    # submission.score_secondary = 0.99
+    # submission.grading_status = "graded"
+    # submission.meta["something"] = "A"
+    # submission.meta["something_else"] = "B"
+    # submission.update(meta_overwrite=False)
+    #
+    # submission_from_server = api.get_submission(challenge_id, submission.id)
+    # assert submission_from_server.score == 0.98
+    # assert submission_from_server.score_secondary == 0.99
+    # assert submission_from_server.grading_status == "graded"
+    # assert submission_from_server.meta["something"] == "A"
+    # assert submission_from_server.meta["something_else"] == "B"
+    #
+    # submission.score = 0.198
+    # submission.score_secondary = 0.199
+    # submission.grading_status = "graded"
+    # submission.meta["something"] = "A_1"
+    # submission.meta["something_else"] = "B_1"
+    # submission.update(meta_overwrite=False)
+    # submission_from_server = api.get_submission(challenge_id, submission.id)
+    # assert submission_from_server.score == 0.198
+    # assert submission_from_server.score_secondary == 0.199
+    # assert submission_from_server.grading_status == "graded"
+    # assert submission_from_server.meta["something"] == "A_1"
+    # assert submission_from_server.meta["something_else"] == "B_1"
+
+
 def test_null_score_secondary_raises_exception():
     """Tests that an exception is raised when score is set and score_secondary
     is not.
