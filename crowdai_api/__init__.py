c5e1b1700425d0e320cc3b9c241b67e0a6006d29
S.P. Mohanty <spmohanty91@gmail.com>
2018-07-10 20:19:09 +0200

    Ignore code exit events as they arer very non deterministic

--- crowdai_api/__init__.py
+++ crowdai_api/__init__.py
@@ -4,18 +4,10 @@
 
 __author__ = """S.P. Mohanty"""
 __email__ = 'spmohanty91@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.9'
 
 from .crowdai_api import API
 from .submission import CrowdAISubmission
 from .gitlab_submission import GitlabSubmission
 from .events import CrowdAIEvents
 from .exceptions import *
-
-import atexit
-def exit_handler():
-    crowdai_events = CrowdAIEvents()
-    crowdai_events.register_event(
-        event_type=CrowdAIEvents.CROWDAI_EVENT_CODE_EXIT
-    )
-atexit.register(exit_handler)
