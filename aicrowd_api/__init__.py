fde62c7824eeb77588ff001bb1ae9feeb2ad429f
Arjun Nemani <nemaniarjun@gmail.com>
2019-08-09 23:08:27 +0530

    Bump version

--- aicrowd_api/__init__.py
+++ aicrowd_api/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """S.P. Mohanty"""
 __email__ = 'mohanty@aicrowd.com'
-__version__ = '0.1.9'
+__version__ = '0.1.23'
 
 from .aicrowd_api import API
 from .submission import AIcrowdSubmission
